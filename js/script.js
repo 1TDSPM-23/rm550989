4d5c0f31e9f10a8216909cfbaba6b60da96e204c
IgorRibeiro7 <igorbez.12@icloud.com>
2023-05-05 09:21:17 -0300

    commit save

---
+++ js/script.js
@@ -0,0 +1,37 @@
+console.log("ola mundo")
+
+var nr1 = 10;
+var nr2 = 5;
+
+console.log(nr1);
+console.log(nr2);
+
+
+console.log("Resultado " + (nr1+nr2));
+console.log("Resultado " + (parseInt(nr1) + parseInt(nr2)))
+console.log(`Resultado Adiçao:  ${parseInt(nr1) + parseInt(nr2)}`);
+
+
+console.log(`Resultado Subtraçao:  ${parseInt(nr1) - parseInt(nr2)}`);
+
+console.log(`Resultado Multiplicaçao: ${parseInt(nr1) * parseInt(nr1)}`);
+
+console.log(`Resultado Subtraçao:  ${parseInt(nr1) - parseInt(nr2)}`)
+
+
+var nr1 = "10";
+var nr2 = "5";
+var resultado;
+
+resultado = !(parseInt(nr1)%parseInt(nr2)) ? "PAR" : "IMPAR";
+console.log(`O NUMERO É : ${resultado}`);
+
+//var nome1 = "Arnaldo";
+//let nome2 = "Jailtom";
+
+var nome = "Juscileu";
+
+if(nome != ""){
+    var nome = "Robson";
+}
+console.log(nome);
